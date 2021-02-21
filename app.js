c1cf094e421ac3e9b68ca32ca243ef29e84466fa
jhigs <flores.gideon1@gmail.com>
2021-02-21 19:17:59 +0800

    Modify Date Identifier

--- app.js
+++ app.js
@@ -9,36 +9,14 @@ app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = '';
 
-  switch (currentDay) {
-    case 0:
-      day = 'Sunday';
-      break;
-    case 1:
-      day = 'Monday';
-      break;
-    case 2:
-      day = 'Teusday';
-      break;
-    case 3:
-      day = 'Wednesday';
-      break;
-    case 4:
-      day = 'Thursday';
-      break;
-    case 5:
-      day = 'Friday';
-      break;
-    case 6:
-      day = 'Saturday';
-      break;
+  var option = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+  };
 
-    default:
-      console.log('Error: Number of day is invalid!');
-      break;
-  }
+  var day = today.toLocaleDateString('en-US', option);
 
   res.render('list', { kindOfDay: day });
 });
