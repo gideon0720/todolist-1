62ec1b1b382dba740c84e94849fcb0aa907e2b70
jhigs <flores.gideon1@gmail.com>
2021-02-22 17:39:48 +0800

    Create a Layout

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+// variables
 let items = ['Give thanks', 'Hygen', 'Breakfast', 'Code'];
+let workItems = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
+
+// Home route
 
 app.get('/', function (req, res) {
   let today = new Date();
@@ -23,14 +27,36 @@ app.get('/', function (req, res) {
 
   let day = today.toLocaleDateString('en-US', option);
 
-  res.render('list', { kindOfDay: day, newItemArray: items });
+  res.render('list', { titleHead: day, newItemArray: items });
 });
 
 app.post('/', function (req, res) {
   let item = req.body.newItem;
-  items.push(item);
 
-  res.redirect('/');
+  console.log(req.body.button);
+  if (req.body.button === 'Work') {
+    workItems.push(item);
+    res.redirect('/work');
+  } else {
+    items.push(item);
+    res.redirect('/');
+  }
+});
+
+// Work route
+
+app.get('/work', function (req, res) {
+  res.render('list', { titleHead: 'Work List', newItemArray: workItems });
+});
+
+app.post('/work', function (req, res) {
+  res.redirect('/work');
+});
+
+// About route
+
+app.get('/about', function (req, res) {
+  res.render('about');
 });
 
 app.listen(3000, function () {
